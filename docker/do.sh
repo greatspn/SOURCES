0d7ee242620605e9b484729895a14a195de875ab
Elvio <elvio@MacM1-Elvio.local>
2021-09-23 12:35:37 +0200

    new dockerfile

--- docker/do.sh
+++ docker/do.sh
@@ -2,7 +2,17 @@
 
 case $1 in
 build)
-	docker build -t greatspn --build-arg update_version="`date`" -f Dockerfile .
+	docker build --target builder --tag greatspn_builder \
+		--build-arg update_version="`date`" -f Dockerfile .
+
+	docker build --target deploy_base --tag greatspn_deploy_base \
+		--build-arg update_version="`date`" -f Dockerfile .
+
+	docker build --target updater --tag greatspn_updater \
+		--build-arg update_version="`date`" -f Dockerfile .
+		
+	docker build --tag greatspn \
+		--build-arg update_version="`date`" -f Dockerfile .
 	;;
 
 run)
