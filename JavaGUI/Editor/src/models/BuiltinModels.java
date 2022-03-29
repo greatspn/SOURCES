3abe79782f6ebf104e44d8ef271faaea33ea490d
Elvio <elvio@macm1elvio-wf.dipinfo.di.unito.it>
2022-03-29 14:42:22 +0200

    experiment for tracking JAR position

--- JavaGUI/Editor/src/models/BuiltinModels.java
+++ JavaGUI/Editor/src/models/BuiltinModels.java
@@ -5,7 +5,10 @@
  */
 package models;
 
+import java.io.File;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  *
@@ -45,5 +48,29 @@ public class BuiltinModels {
     
     public static InputStream getModelStream(String model) {
         return BuiltinModels.class.getResourceAsStream("/models/" + model + ".PNPRO");
+    }
+    
+    public static void main(String[] args) {
+        try {
+            String path = BuiltinModels.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            String decodedPath = URLDecoder.decode(path, "UTF-8");
+            System.out.println("decodedPath="+decodedPath);
+            File file = new File(decodedPath);
+            if (file.isFile() && file.exists() && decodedPath.endsWith(".jar")) {
+                // Running from JAR
+                System.out.println(file.getParent());
+            }
+            else if (file.isDirectory() && file.exists() && 
+                    decodedPath.endsWith("JavaGUI/Editor/build/classes/")) 
+            {
+                // Running from class sources
+            }
+            else {
+                // Unknown
+            }
+        }
+        catch (UnsupportedEncodingException e) {
+            
+        }
     }
 }
