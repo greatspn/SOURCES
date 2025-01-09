392f4b14985e20d6380b631e4448916488a4b313
Mandringo <mandringo.duregon@gmail.com>
2025-01-09 16:30:43 +0100

    updated build_lpsolve.sh for epimod's docker

--- contrib/build_lpsolve.sh
+++ contrib/build_lpsolve.sh
@@ -1,87 +1,45 @@
 #!/bin/bash
 
-# Script to build lp_solve from sources on Linux and macOS.
-# The folder of the lp_solve sources are expected to be in the cwd.
 LIBSOLVE55_DIR=lp_solve_5.5
 if [ ! -d "${LIBSOLVE55_DIR}" ]; then
-	echo "Missing LIBSOLVE55_DIR directory(${LIBSOLVE55_DIR})."
-	exit 1
+  echo "Missing LIBSOLVE55_DIR directory(${LIBSOLVE55_DIR})."
+  exit 1
 fi
-
-# Determine which os-dependent script to run.
-#case "$OSTYPE" in
-#  solaris*) 
-#	echo "SOLARIS"
-#	exit 1 
-#	;;
-#  darwin*)  
-#	echo "OSX"
-#	RUN=ccc.osx
-#	;;
-#  linux*)   
-#	echo "LINUX"
-#	RUN=ccc
-#	;;
-#  bsd*)     
-#	echo "BSD"
-#	exit 1 
-#	;;
-#  msys*)    
-#	echo "WINDOWS"
-#	exit 1 
-#	;;
-#  cygwin*)  
-#	echo "CYGWIN"
-#	RUN=ccc
-#	exit 1 
-#	;;
-#  *)        
-#	echo "unknown: $OSTYPE"
-#	exit 1 
-#	;;
-#esac
 
 if [ "$(uname)" = "Darwin" ]; then
-	echo "OSX"
-	RUN=ccc.osx
+  echo "OSX"
+  RUN=ccc.osx
 elif [ "$(uname)" = "Linux" ]; then
-	echo "LINUX"
-	RUN=ccc
+  echo "LINUX"
+  RUN=ccc
 else
-	echo "Unsupported OS: $(uname)"
-	exit 1
+  echo "Unsupported OS: $(uname)"
+  exit 1
 fi
 
-echo build lpsolve lib
+echo "Compilazione di lpsolve lib"
 (
-	cd ${LIBSOLVE55_DIR}/lpsolve55
-	sh ${RUN}
+  cd ${LIBSOLVE55_DIR}/lpsolve55
+  sh ${RUN} || (echo "Errore durante la compilazione di lpsolve!" && exit 1)
 )
 
-echo build colamd
+echo "Compilazione di colamd"
 (
-	cd ${LIBSOLVE55_DIR}/colamd
-	gcc -c colamd.c -o colamd.o
-	ar rcs libcolamd.a  colamd.o
+  cd ${LIBSOLVE55_DIR}/colamd
+  gcc -c colamd.c -o colamd.o || (echo "Errore durante la compilazione di colamd!" && exit 1)
+  ar rcs libcolamd.a colamd.o || (echo "Errore durante la creazione di libcolamd.a!" && exit 1)
 )
 
-echo install
+echo "Installazione delle librerie"
 (
-	# only copy static libraries, to improve portability
-	sudo cp ${LIBSOLVE55_DIR}/colamd/libcolamd.a \
-			${LIBSOLVE55_DIR}/lpsolve55/bin/*/liblpsolve55.a \
-			/usr/local/lib/
-	# copy headers
-	sudo cp ${LIBSOLVE55_DIR}/*.h lp_solve_5.5/colamd/colamd.h \
-			/usr/local/include/
+  cp ${LIBSOLVE55_DIR}/colamd/libcolamd.a \
+     ${LIBSOLVE55_DIR}/lpsolve55/bin/*/liblpsolve55.a \
+     /usr/local/lib/ || (echo "Errore durante la copia delle librerie!" && exit 1)
+  cp ${LIBSOLVE55_DIR}/*.h ${LIBSOLVE55_DIR}/colamd/colamd.h \
+     /usr/local/include/ || (echo "Errore durante la copia degli header!" && exit 1)
 )
-echo ok.
 
-
-
-
-
-
-
-
+echo "Verifica finale"
+ls -l /usr/local/lib/libcolamd.a || (echo "libcolamd.a non trovato!" && exit 1)
+echo "Build completata correttamente."
 
