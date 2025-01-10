5ab05f0fedde5716ceb5fa521021c5d97fdcd351
Mandringo <mandringo.duregon@gmail.com>
2025-01-10 14:07:34 +0100

    build

--- contrib/build_lpsolve.sh
+++ contrib/build_lpsolve.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
-LIBSOLVE55_DIR=${1:-lp_solve_5.5}
+# Script to build lp_solve from sources on Linux and macOS.
+# The folder of the lp_solve sources are expected to be in the cwd.
+LIBSOLVE55_DIR=lp_solve_5.5
 if [ ! -d "${LIBSOLVE55_DIR}" ]; then
-  echo "Missing LIBSOLVE55_DIR directory(${LIBSOLVE55_DIR})."
-  exit 1
+	echo "Missing LIBSOLVE55_DIR directory(${LIBSOLVE55_DIR})."
+	exit 1
 fi
+
+# Determine which os-dependent script to run.
+#case "$OSTYPE" in
+#  solaris*) 
+#	echo "SOLARIS"
+#	exit 1 
+#	;;
+#  darwin*)  
+#	echo "OSX"
+#	RUN=ccc.osx
+#	;;
+#  linux*)   
+#	echo "LINUX"
+#	RUN=ccc
+#	;;
+#  bsd*)     
+#	echo "BSD"
+#	exit 1 
+#	;;
+#  msys*)    
+#	echo "WINDOWS"
+#	exit 1 
+#	;;
+#  cygwin*)  
+#	echo "CYGWIN"
+#	RUN=ccc
+#	exit 1 
+#	;;
+#  *)        
+#	echo "unknown: $OSTYPE"
+#	exit 1 
+#	;;
+#esac
 
 if [ "$(uname)" = "Darwin" ]; then
   echo "OSX"
@@ -16,6 +51,7 @@ else
   echo "Unsupported OS: $(uname)"
   exit 1
 fi
+
 
 echo build lpsolve lib
 (
@@ -41,6 +77,9 @@ echo install
 			/usr/local/include/
 )
 echo ok.
+
+
+
 
 
 
